a96c1452c53a9cfbeec731d7d7b58f3739db248b
Olaf Haag <olaf@wolf3d.io>
2023-06-26 01:30:01 +0200

    feat(schema): add python BaseModel
    add $schema and $id fields to json, remove property titles

---
+++ src/readyplayerme/asset_validation_schemas/basemodel.py
@@ -0,0 +1,30 @@
+from typing import Any
+
+from pydantic import BaseConfig, Extra
+from pydantic import BaseModel as PydanticBaseModel
+
+
+class SchemaConfig(BaseConfig):
+    extra = Extra.forbid
+    validate_assignment = True
+    validate_all = True
+    anystr_strip_whitespace = True
+
+    @staticmethod
+    def schema_extra(schema: dict[str, Any], model: type["PydanticBaseModel"]) -> None:
+        # Add metaschema and id.
+        schema |= {
+            "$schema": "https://json-schema.org/draft/2020-12/schema",
+            # Get the "outer" class name with a lower case first letter.
+            "$id": f"{(name := model.__name__)[0].lower() + name[1:]}.schema.json",
+        }
+        # Remove "title" from properties.
+        for prop in schema.get("properties", {}).values():
+            prop.pop("title", None)
+
+
+class BaseModel(PydanticBaseModel):
+    """Global base class for all models."""
+
+    class Config(SchemaConfig):
+        ...
