6bf25f90babf7a9f9146e9adb054cd3cf4433625
Olaf Haag <olaf@wolf3d.io>
2023-08-08 03:16:18 +0200

    fix(schema)!: update mesh_triangle_count to v2

--- src/readyplayerme/asset_validation_schemas/mesh_triangle_count.py
+++ src/readyplayerme/asset_validation_schemas/mesh_triangle_count.py
@@ -1,15 +1,24 @@
-from typing import Any, ClassVar
+"""Triangle count validation pydantic model."""
+from typing import Annotated, Any
 
 from pydantic import BaseModel as PydanticBaseModel
 from pydantic import (
+    Field,
     ValidationError,
-    conint,
+    ValidationInfo,
+    ValidatorFunctionWrapHandler,
     create_model,
 )
+from pydantic.functional_validators import WrapValidator
+from pydantic_core import ErrorDetails, InitErrorDetails, PydanticCustomError
 
-from readyplayerme.asset_validation_schemas.basemodel import SchemaConfig
+from readyplayerme.asset_validation_schemas.basemodel import get_model_config
 
 ERROR_CODE = "TRIANGLE_COUNT"
+DOCS_URL = (
+    "https://docs.readyplayer.me/asset-creation-guide/validation/validation-checks/"
+    "mesh-validations/check-mesh-triangle-count"
+)
 
 # Triangle budgets could be set via config.
 limits = {
@@ -22,62 +31,112 @@ limits = {
     "glasses": 1000,
     "hair": 3000,
     "head": 4574,
-    "headCustom": 6000,
+    "head_custom": 6000,
     "headwear": 2500,
-    "outfitBottom": 6000,
-    "outfitTop": 6000,
-    "outfitFootwear": 2000,
-    "halfbodyShirt": 1000,
+    "outfit_bottom": 6000,
+    "outfit_top": 6000,
+    "outfit_footwear": 2000,
+    "halfbody_shirt": 1000,
     "teeth": 1000,
 }
 
 
+def convert_error(error: ErrorDetails, title: str = "Unknown Error", url: str = ""):
+    """Convert the error to a PydanticCustomError with a custom error type and message."""
+    match error:
+        case {"type": "greater_than"}:
+            raise PydanticCustomError(
+                ERROR_CODE,
+                "Mesh must have at least 1 triangle."
+                # Include the URL if it's not empty. Indent by tab (4 spaces).
+                + "\n\tFor further information visit {url}.".expandtabs(4) * bool(url),
+                {"url": url},
+            )
+        case {"type": "less_than_equal"}:
+            raise PydanticCustomError(
+                ERROR_CODE,
+                "Mesh exceeds triangle count budget. Allowed: {limit}. Found: {wrong_value}."
+                + "\n\tFor further information visit {url}.".expandtabs(4) * bool(url),
+                {"limit": error["ctx"]["le"], "wrong_value": error["input"], "url": url},
+            )
+        case _:  # Anything else raise ValidationError.
+            # Convert ErrorDetails to InitErrorDetails
+            ctx = error.get("ctx", {})
+            init_error_details: InitErrorDetails = InitErrorDetails(
+                type=error["type"],
+                loc=error["loc"],
+                input=error["input"],
+                ctx=ctx,
+            )
+            raise ValidationError.from_exception_data(title=title, line_errors=[init_error_details])
+
+
+def validate_tricount(value: Any, handler: ValidatorFunctionWrapHandler, _info: ValidationInfo) -> int:
+    """Wrap the validation function to raise custom error types.
+
+    Return the validated value if no error occurred.
+    """
+    try:
+        return handler(value)
+    except ValidationError as error:
+        for err in error.errors():
+            convert_error(err, title=error.title, url=DOCS_URL)
+        return value  # This line is unreachable, but makes type-checkers happy.
+
+
+def get_triangle_count_type(le: int) -> Annotated:
+    """Return a constrained positive integer field type with custom error messages.
+
+    :param le: The (inclusive) maximum of the integer (less-equal).
+    """
+    return Annotated[
+        int,
+        Field(
+            gt=0,
+            le=le,
+            json_schema_extra={
+                "errorMessage": {
+                    "exclusiveMinimum": "Mesh ${1/name} must have at least 1 triangle.",
+                    # Use % formatting for error message instead of f-string, since we need to preserve "${0}".
+                    "maximum": "Mesh ${1/name} exceeds triangle count budget. Allowed: %d. Found: ${0}." % le,
+                }
+            },
+        ),
+        WrapValidator(validate_tricount),
+    ]
+
+
 def get_tricount_field_definitions(limits: dict[str, int]) -> Any:
-    """Turn simple integer limits into a dict of constrained positive integer field types."""
-    return {field_name: (conint(gt=0, le=limit), None) for field_name, limit in limits.items()}
-
-
-class TriCountConfig(SchemaConfig):
-    """Triangle count schema title and error messages."""
-
-    title = "Triangle Count"
-    error_msg_templates: ClassVar[dict[str, str]] = {
-        # Prepend the error code to the messages.
-        e: f"{ERROR_CODE} " + msg
-        for e, msg in {
-            "value_error.number.not_gt": "Mesh must have at least 1 triangle.",
-            "value_error.number.not_le": "Mesh exceeds triangle count budget. Allowed: {limit_value}.",
-            "value_error.extra": "Invalid extra mesh.",
-        }.items()
+    """Turn simple integer limits into a dict of constrained positive integer field types with custom error messages."""
+    return {
+        field_name: (  # Tuple of (type definition, default value).
+            get_triangle_count_type(limit),
+            None,  # Default value.
+        )
+        for field_name, limit in limits.items()
     }
-
-    @classmethod  # As a staticmethod, TypeError is raised with super().schema_extra args. Works with classmethod.
-    def schema_extra(cls, schema: dict[str, Any], model: type["PydanticBaseModel"]) -> None:
-        super().schema_extra(schema, model)
-        # Add custom ajv-error messages to the schema.
-        for prop in schema.get("properties", {}).values():
-            prop["errorMessage"] = {
-                "exclusiveMinimum": "Mesh ${1/name} must have at least 1 triangle.",
-                "maximum": "Mesh ${1/name} exceeds triangle count budget. Allowed: %d. Found: ${0}." % prop["maximum"],
-            }
 
 
 # Dynamically create the model with the tri-count limits we defined earlier.
 MeshTriangleCountModel: type[PydanticBaseModel] = create_model(
     "MeshTriangleCount",
-    __config__=TriCountConfig,
+    __config__=get_model_config(title="Triangle Count", validate_default=False),
     # Populate the fields.
     **get_tricount_field_definitions(limits),
 )
 
 
 if __name__ == "__main__":
+    import json
+    import logging
+
+    logging.basicConfig(level=logging.DEBUG)
     # Convert model to JSON schema.
-    print(MeshTriangleCountModel.schema_json(indent=2))
+    logging.debug(json.dumps(MeshTriangleCountModel.model_json_schema(), indent=2))
 
     # Example of validation in Python.
     try:
         # Multiple checks at once. Test non-existent field as well.
         MeshTriangleCountModel(outfitBottom=6000, outfitTop=0, outfitFootwear=3000, foo=10)
-    except ValidationError as error:
-        print("\nValidation Errors:\n", error)
+    except (PydanticCustomError, ValidationError, TypeError) as error:
+        logging.error("\nValidation Errors:\n %s", error)
