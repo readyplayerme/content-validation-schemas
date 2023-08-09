370f2e06ee34e6fafd4376d636c5f72ea8df2f2c
Iván Sanandrés <43515426+Ivan-Sanandres@users.noreply.github.com>
2023-08-09 18:42:34 +0200

    fix(schema): updated material_names model

--- src/readyplayerme/asset_validation_schemas/material_names.py
+++ src/readyplayerme/asset_validation_schemas/material_names.py
@@ -1,105 +1,158 @@
-from typing import Any, ClassVar, List, Literal
+from enum import Enum
+from typing import Annotated, Any, Literal
 
 from pydantic import BaseModel as PydanticBaseModel
-from pydantic import ValidationError, constr, create_model
+from pydantic import (
+    Field,
+    FieldValidationInfo,
+    ValidationError,
+    ValidatorFunctionWrapHandler,
+    create_model,
+    field_validator,
+)
+from pydantic.functional_validators import WrapValidator
+from pydantic_core import PydanticCustomError
 
-from readyplayerme.asset_validation_schemas.basemodel import SchemaConfig
+from readyplayerme.asset_validation_schemas.basemodel import get_model_config
+
+material_names = {
+    "beard": "Wolf3D_Beard",
+    "body": "Wolf3D_Body",
+    "facewear": "Wolf3D_Facewear",
+    "glasses": "Wolf3D_Glasses",
+    "hair": "Wolf3D_Hair",
+    "halfBodyShirt": "Wolf3D_Shirt",
+    "head": "Wolf3D_Skin",
+    "eye": "Wolf3D_Eye",
+    "teeth": "Wolf3D_Teeth",
+    "headwear": "Wolf3D_Headwear",
+    "bottom": "Wolf3D_Outfit_Bottom",
+    "footwear": "Wolf3D_Outfit_Footwear",
+    "top": "Wolf3D_Outfit_Top",
+}
 
 
-class MaterialNamesConfig(SchemaConfig):
-    """Material Names schema title and error messages."""
+class OutfitMaterialNames(str, Enum):
+    bottom = material_names["bottom"]
+    footwear = material_names["footwear"]
+    top = material_names["top"]
+    body = material_names["body"]
 
-    title = "Material Names"
-    error_msg_templates: ClassVar[dict[str, str]] = {
-        # Prepend the error code to the messages.
-        "literal": "Material name should be '${const}'. Found ${0} instead.",
-        "enum": "Material name should be one of ${allowed_values}. Found ${0} instead.",
+
+class HeroAvatarMaterialNames(str, Enum):
+    bottom = material_names["bottom"]
+    footwear = material_names["footwear"]
+    top = material_names["top"]
+    body = material_names["body"]
+    head = material_names["head"]
+    eye = material_names["eye"]
+    teeth = material_names["teeth"]
+    hair = material_names["hair"]
+    beard = material_names["beard"]
+    facewear = material_names["facewear"]
+    glasses = material_names["glasses"]
+    headwear = material_names["headwear"]
+
+
+ERROR_CODE = "MATERIAL_NAMES"
+DOCS_URL = "https://docs.readyplayer.me/asset-creation-guide/validation/validation-checks/"
+
+
+def get_error_type_msg(field_name: str, value: Any) -> tuple[str, str] | tuple[None, None]:
+    """Convert the error to a custom error type and message.
+
+    If the error type is not covered, return a None-tuple.
+    """
+    match field_name:
+        case key if key in material_names:
+            return ERROR_CODE, f"Material name should be '{material_names[key]}'. Found '{value}' instead." + f"\n\tFor further information visit {DOCS_URL}.".expandtabs(4) * bool(DOCS_URL)
+        case "outfit":
+            return ERROR_CODE, f"Material name should be one of {', '.join(OutfitMaterialNames)}. Found '{value}' instead." + f"\n\tFor further information visit {DOCS_URL}.".expandtabs(4) * bool(DOCS_URL)
+        case key if key in ("non_customizable_avatar", "nonCustomizableAvatar"):
+            return ERROR_CODE, f"Material name should be one of {', '.join(HeroAvatarMaterialNames)}. Found '{value}' instead." + f"\n\tFor further information visit {DOCS_URL}.".expandtabs(4) * bool(DOCS_URL)
+        case _:
+            return None, None
+
+
+def custom_error_validator(value: Any, handler: ValidatorFunctionWrapHandler, info: FieldValidationInfo) -> Any:
+    """Wrap the field validation function to raise custom error types.
+
+    Return the validated value if no error occurred.
+    """
+    try:
+        return handler(value)
+    except ValidationError as error:
+        for err in error.errors():
+            error_type, error_msg = get_error_type_msg(info.field_name, err["input"])
+            if not error_type or error_msg:  # We didn't cover this error, so raise default.
+                raise
+            raise PydanticCustomError(error_type, error_msg) from error
+
+
+def get_material_name_type(material_name: str) -> Annotated:
+    """Return a constrained positive integer field type with custom error messages.
+
+    :param le: The (inclusive) maximum of the integer (less-equal).
+    """
+    return Annotated[
+        Literal[material_name],
+        Field(json_schema_extra={"errorMessage": "Material name should be '%s'. Found ${0} instead." % material_name})
+    ]
+
+
+def get_material_name_field_definitions(material_names: dict[str, str]) -> Any:
+    """Turn simple integer limits into a dict of constrained positive integer field types with custom error messages."""
+    return {
+        field_name: (  # Tuple of (type definition, default value).
+            get_material_name_type(material_name),
+            None,  # Default value.
+        )
+        for field_name, material_name in material_names.items()
     }
 
-    @classmethod
-    def schema_extra(cls, schema: dict[str, Any], model: type[PydanticBaseModel]) -> None:
-        super().schema_extra(schema, model)
-        # Add custom ajv-error messages to the schema.
-        for prop in schema.get("properties", {}).values():
-            if "literal" in prop:
-                prop["errorMessage"] = cls.error_msg_templates["literal"].replace("${const}", prop["literal"])
-            elif "enum" in prop:
-                prop["errorMessage"] = cls.error_msg_templates["enum"].replace("${allowed_values}", str(prop["enum"]))
 
-class MaterialNames(SchemaConfig):
-    beard: Literal["Wolf3D_Beard"]
-    facewear: Literal["Wolf3D_Facewear"]
-    glasses: Literal["Wolf3D_Glasses"]
-    hair: Literal["Wolf3D_Hair"]
-    halfBodyShirt: Literal["Wolf3D_Shirt"]
-    head: Literal["Wolf3D_Skin"]
-    headwear: Literal["Wolf3D_Headwear"]
-    modularBottom: Literal["Wolf3D_Outfit_Bottom"]
-    modularFootwear: Literal["Wolf3D_Outfit_Footwear"]
-    modularTop: Literal["Wolf3D_Outfit_Top"]
-    nonCustomizableAvatar: List[Literal[
-        "Wolf3D_Body",
-        "Wolf3D_Eye",
-        "Wolf3D_Outfit_Bottom",
-        "Wolf3D_Outfit_Footwear",
-        "Wolf3D_Outfit_Top",
-        "Wolf3D_Skin",
-        "Wolf3D_Teeth",
-        "Wolf3D_Hair",
-        "Wolf3D_Beard",
-        "Wolf3D_Facewear"
-        "Wolf3D_Glasses"
-        "Wolf3D_Headwear"
-    ]]
-    outfit: List[Literal[
-        "Wolf3D_Body",
-        "Wolf3D_Outfit_Bottom",
-        "Wolf3D_Outfit_Footwear",
-        "Wolf3D_Outfit_Top",
-    ]]
+outfit_field = Annotated[
+    OutfitMaterialNames,
+    Field(
+        json_schema_extra={
+            "errorMessage": "Material name should be one of %s. Found ${0} instead." % ", ".join(OutfitMaterialNames)
+        }
+    )
+]
 
-MaterialNamesModel = create_model(
+hero_avatar_field = Annotated[
+    HeroAvatarMaterialNames,
+    Field(
+        json_schema_extra={
+            "errorMessage": "Material name should be one of %s. Found ${0} instead."
+            % ", ".join(HeroAvatarMaterialNames)
+        }
+    )
+]
+
+# Wrap all field validators in a custom error validator.
+val_wrap = field_validator("*", mode="wrap")(custom_error_validator)
+
+MaterialNamesModel: type[PydanticBaseModel] = create_model(
     "MaterialNames",
-    beard=(Literal["Wolf3D_Beard"], ...),
-    facewear=(Literal["Wolf3D_Facewear"], ...),
-    glasses=(Literal["Wolf3D_Glasses"], ...),
-    hair=(Literal["Wolf3D_Hair"], ...),
-    halfBodyShirt=(Literal["Wolf3D_Shirt"], ...),
-    head=(Literal["Wolf3D_Skin"], ...),
-    headwear=(Literal["Wolf3D_Headwear"], ...),
-    modularBottom=(Literal["Wolf3D_Outfit_Bottom"], ...),
-    modularFootwear=(Literal["Wolf3D_Outfit_Footwear"], ...),
-    modularTop=(Literal["Wolf3D_Outfit_Top"], ...),
-    nonCustomizableAvatar=(List[Literal[
-        "Wolf3D_Body",
-        "Wolf3D_Eye",
-        "Wolf3D_Outfit_Bottom",
-        "Wolf3D_Outfit_Footwear",
-        "Wolf3D_Outfit_Top",
-        "Wolf3D_Skin",
-        "Wolf3D_Teeth",
-        "Wolf3D_Hair",
-        "Wolf3D_Beard",
-        "Wolf3D_Facewear"
-        "Wolf3D_Glasses"
-        "Wolf3D_Headwear"
-    ]], ...),
-    outfit=(List[Literal[
-        "Wolf3D_Body",
-        "Wolf3D_Outfit_Bottom",
-        "Wolf3D_Outfit_Footwear",
-        "Wolf3D_Outfit_Top",
-    ]], ...),
-    __config__=MaterialNamesConfig,
+    __config__=get_model_config(title="Material Names", validate_default=False),
+    __validators__={"*": val_wrap},
+    **get_material_name_field_definitions(material_names),
+    outfit=(outfit_field, None),
+    non_customizable_avatar=(hero_avatar_field, None),
 )
 
 
 if __name__ == "__main__":
+    import json
+    import logging
+
+    logging.basicConfig(level=logging.DEBUG)
     # Convert model to JSON schema.
-    print(MaterialNamesModel.schema_json(indent=2))
+    logging.debug(json.dumps(MaterialNamesModel.model_json_schema(), indent=2))
     # Example of validation in Python.
     try:
         # Test example validation.
-        MaterialNamesModel(beard="Wrong_Material_Name")
-    except ValidationError as error:
-        print("\nValidation Errors:\n", error)
+        MaterialNamesModel(beard="Wrong_Material_Name", outfit="Other_Wrong_Material_Name")
+    except (PydanticCustomError, ValidationError, TypeError) as error:
+        logging.error("\nValidation Errors:\n %s", error)
