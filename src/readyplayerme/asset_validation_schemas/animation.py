74415718c3a3ccfe2184203438f88b457aebd557
Olaf Haag <olaf@wolf3d.io>
2023-08-24 17:52:48 +0200

    refactor(schema): animation
    use moved functions that were moved earlier

--- src/readyplayerme/asset_validation_schemas/animation.py
+++ src/readyplayerme/asset_validation_schemas/animation.py
@@ -1,9 +1,14 @@
 """Sub-schemas for animation validation."""
+from pathlib import Path
+from typing import Any
+
 from pydantic import Field, ValidationError, field_validator
+from pydantic.alias_generators import to_camel
 from pydantic.dataclasses import dataclass
 from pydantic_core import ErrorDetails
 
-from readyplayerme.asset_validation_schemas.schema_io import properties_comment
+from readyplayerme.asset_validation_schemas.basemodel import get_model_config
+from readyplayerme.asset_validation_schemas.schema_io import add_metaschema, properties_comment, remove_keys_from_schema
 from readyplayerme.asset_validation_schemas.validators import CustomValidator, ErrorMsgReturnType
 
 ANIMATION_ERROR = "AnimationError"
@@ -15,7 +20,15 @@ def error_msg_func(field_name: str, error_details: ErrorDetails) -> ErrorMsgRetu
     return ANIMATION_ERROR, ANIMATION_ERROR_MSG
 
 
-@dataclass
+def json_schema_extra(schema: dict[str, Any]) -> None:
+    """Provide extra JSON schema properties."""
+    # Add metaschema and id.
+    add_metaschema(schema)
+    schema["$id"] = f"{to_camel(Path(__file__).stem)}.schema.json"
+    remove_keys_from_schema(schema, ["title", "default"])
+
+
+@dataclass(config=get_model_config(title="Animation", json_schema_extra=json_schema_extra))
 class NoAnimation:
     """Empty animation data."""
 
@@ -36,16 +49,13 @@ if __name__ == "__main__":
     from pydantic.json_schema import model_json_schema
     from pydantic_core import PydanticCustomError
 
-    from readyplayerme.asset_validation_schemas.basemodel import SchemaNoTitleAndDefault
     from readyplayerme.asset_validation_schemas.schema_io import write_json
 
     logging.basicConfig(encoding="utf-8", level=logging.DEBUG)
 
     # Demonstrate alternative way to convert a model to custom JSON schema.
-    top_level_schema = model_json_schema(
-        NoAnimation, schema_generator=SchemaNoTitleAndDefault  # type: ignore[arg-type]
-    )
-    write_json(top_level_schema)
+    schema = model_json_schema(NoAnimation)  # type: ignore[arg-type]
+    write_json(schema)
 
     # Example of validation in Python.
     try:
