79276b83550f99f1864abdaefe415f5ba61a9ce7
Olaf Haag <olaf@wolf3d.io>
2023-08-24 20:31:20 +0200

    feat(schema): custom StrEnum type
    Python <3.11 doesn't yet support it in the stdlib

--- src/readyplayerme/asset_validation_schemas/types.py
+++ src/readyplayerme/asset_validation_schemas/types.py
@@ -4,8 +4,21 @@ from enum import Enum
 from typing import cast
 
 
+class StrEnum(str, Enum):
+    """Enum with string values.
+
+    This is a workaround for Python 3.10 not yet supporting StrEnum.
+    """
+
+    def __str__(self) -> str:
+        return self.value
+
+    def __repr__(self) -> str:
+        return str.__repr__(self.value)
+
+
 # Instead of spelling out members and values for each enum, create classes dynamically.
-def create_enum_class(name: str, dictionary: dict[str, str], keys: Container[str] | None = None) -> Enum:
+def create_enum_class(name: str, dictionary: dict[str, str], keys: Container[str] | None = None) -> StrEnum:
     """Create an string-enum class from a dictionary.
 
     If keys are provided, only the keys will be included in the enum class.
@@ -18,7 +31,7 @@ def create_enum_class(name: str, dictionary: dict[str, str], keys: Container[str
         members = dictionary
     else:
         members = dict(filter(is_key_set, dictionary.items()))
-    return Enum(name, members, type=str)
+    return StrEnum(name, members)  # type: ignore[call-overload]
 
 
 def get_enum_length(enum: Enum) -> int:
